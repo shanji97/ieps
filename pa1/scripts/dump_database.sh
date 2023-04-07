8f027918ff7409dc285177b25e0d86895c713b36
shanji97 <aleksander.kovac97@hotmail.com>
2023-04-07 23:42:37 +0200

    .

--- pa1/scripts/dump_database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm pa1/database/crawldb-*
-PGPASSWORD="5loFzWry6ZwC" pg_dump -h ep-falling-block-598917.eu-central-1.aws.neon.tech -Fc -U orglce neondb > pa1/database/crawldb-$(date +"%Y_%m_%d_%I_%M").dump
-
