b3a9b12245c092ff0cf3b94b73165913c195ad6f
Luka Dragar <dragarluka@gmail.com>
2023-05-02 13:07:06 +0200

    Updates to the clean html function

--- pa2/utils.py
+++ pa2/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from bs4 import Comment, BeautifulSoup
 
 
@@ -5,12 +7,12 @@ def clean_html(html):
 
     soup = BeautifulSoup(html, features='lxml')
     for s in soup.select('script'):
-        s.extract()
+        s.replace_with(soup.new_tag('script', type='text/javascript'))
     comments = soup.find_all(string=lambda text: isinstance(text, Comment))
     for c in comments:
-        c.extract()
+        c.replace_with(soup.new_tag('div', **{'class': 'html_comment'}))
     for s in soup.select('style'):
-        s.extract()
+        s.replace_with(soup.new_tag('style', type='text/css'))
     cleaned_soup_html = str(BeautifulSoup(str(soup), features='lxml'))
 
     new_html = ""
@@ -26,7 +28,10 @@ def clean_html(html):
     new_html_joined = []
     i = 0
     while i < len(new_html) - 1:
-        if new_html[i].startswith(("<div", "<button")) and new_html[i + 1] in ["</div>", "</button>"]:
+        tag_name = re.match(r'<\s*([a-zA-Z0-9]+).*?>', new_html[i])
+        if tag_name:
+            tag_name = tag_name.group(1)
+        if tag_name and new_html[i].startswith(("<" + tag_name)) and new_html[i + 1] in ["</" + tag_name + ">"]:
             new_html_joined.append(new_html[i] + new_html[i + 1])
             i += 2
         else:
