7f73b1c37b9bddea030c69b1aba5cf21383313ce
matej2000 <matejvatovec22@gmail.com>
2023-05-07 13:19:02 +0200

    Road_runner

---
+++ pa2/implementation-extraction/road_runner.py
@@ -0,0 +1,164 @@
+from utils import *
+import sys
+sys.setrecursionlimit(4000)
+
+
+#first_html = open("./pa2/pages/overstock.com/jewelry01.html", 'r').read()
+#second_html = open("./pa2/pages/overstock.com/jewelry02.html", 'r').read()
+
+first_html = open("./pa2/input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html", 'r').read()
+second_html = open("./pa2/input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html", 'r').read()
+
+#first_html = open("./pa2/pages/example1.html", 'r').read()
+#second_html = open("./pa2/pages/example2.html", 'r').read()
+
+first_wrapper = simplify(clean_html(remove_script(first_html)))
+open("./pa2/pages/example1_wrapper.html", 'w').write(first_wrapper)
+first_wrapper_lines = first_wrapper.split("\n")
+second_html = simplify(clean_html(remove_script(second_html)))
+second_html = second_html.split("\n")
+
+def is_opening(line):
+    if isinstance(line,str):
+        if line.startswith("</"):
+            return False
+        elif line.startswith("<"):
+            return True
+        return False
+    return False
+
+def is_closing(line):
+    if isinstance(line,str):
+        if line.startswith("</"):
+            return True
+        elif line.startswith("<"):
+            return False
+        return False
+    return False
+
+def find_end(wrapper, start):
+    i = start
+    while i < len(wrapper):
+        if is_closing(wrapper[i]) and wrapper[i][2:] == wrapper[start][1:]:
+            return i
+        i += 1
+    return -1
+
+def find_start(wrapper, start):
+    i = start
+    while i>0:
+        if is_opening(wrapper[i]) and wrapper[i][1:] == wrapper[start][2:]:
+            return i
+        i -= 1
+    return -1
+
+def get_tag(line):
+    if is_opening(line):
+        return line[1:-1]
+    return line[2:-1]
+
+def is_optional(line):
+    if isinstance(line, str) and line.startswith("(opt)"):
+        return True
+    return False
+                 
+def combine_iterator(wrapper, iterator_tag, iterator):
+    i = len(wrapper)-1
+    end = None
+    while i>0:
+        while is_optional(wrapper[i]):
+            i -= 1
+        if is_closing(wrapper[i]) and get_tag(wrapper[i]) == get_tag(iterator_tag):
+            while i>0:
+                if is_opening(wrapper[i]) and get_tag(wrapper[i]) == get_tag(iterator_tag):
+                    end = i
+                    i -=1
+                    break
+                i -=1
+        else:
+            break
+    if end is None:
+        return wrapper
+    
+    wrapper = wrapper[:end]
+    wrapper.append(iterator)
+    return wrapper
+
+def match(wrapper, sample, new_wrapper, start1, start2):
+    if start1== len(wrapper) or start2 == len(sample):
+        return new_wrapper
+    wrapper_line = wrapper[start1]
+    sample_line = sample[start2]
+    if wrapper_line == sample_line:
+        new_wrapper.append(wrapper_line)
+        return match(wrapper, sample, new_wrapper, start1+1, start2+1)
+    elif wrapper_line != sample_line and not(wrapper_line.startswith("<") and sample_line.startswith("<")):
+        new_wrapper.append(".*")
+        return match(wrapper, sample, new_wrapper, start1+1, start2+1)
+    else:
+        #Tag missmatch: is iterator:
+        terminal_wrapper = wrapper[start1-1]
+        terminal_sample = sample[start2-1]
+        found = False
+        if is_closing(terminal_wrapper) and is_opening(wrapper_line) and  get_tag(terminal_wrapper) == get_tag(wrapper_line):
+            end = find_end(wrapper, start1)
+            if end != -1:
+                start = find_start(wrapper, start1-1)
+                if start != -1:
+                    iter1 = wrapper[start:start1]
+                    iter2 = wrapper[start1:end+1]
+                    iterator = match(iter1, iter2, [], 0, 0)
+                    if iterator is not None:
+                        found = True
+                        comb = combine_iterator(new_wrapper, terminal_wrapper, iterator)
+                        a=0
+                        return match(wrapper, sample, comb, end+1, start2)
+        elif is_closing(terminal_sample) and is_opening(sample_line) and  get_tag(terminal_sample) == get_tag(sample_line):
+            end = find_end(sample, start2)
+            if end != -1:
+                start = find_start(sample, start2-1)
+                if start != -1:
+                    iter1 = sample[start:start2]
+                    iter2 = sample[start2:end+1]
+                    iterator = match(iter1, iter2, [], 0, 0)
+                    if iterator is not None:
+                        found = True
+                        comb = combine_iterator(new_wrapper, terminal_sample, iterator)
+                        return match(wrapper, sample, comb, start1, end+1)
+        #is optional
+        if not found:
+            i1 = 100000
+            i2 = 100000
+            for i in range(start1, len(wrapper)):
+                if wrapper[i] == sample_line:
+                    i1 = i
+                    break
+            for i in range(start2, len(sample)):
+                if sample[i] == wrapper_line:
+                    i2 = i
+                    break  
+            if i1 == i2:
+                return None
+            elif i1<i2:
+                for i in range(start1, i1):
+                    new_wrapper.append("(opt) ("+wrapper[i]+")?")
+                return match(wrapper, sample, new_wrapper, i1, start2)
+            elif i1>i2:
+                for i in range(start2, i2):
+                    new_wrapper.append("(opt) ("+sample[i]+")?")
+                return match(wrapper, sample, new_wrapper, start1, i2)
+            return None
+        
+def to_regex(iterator):
+    output = "("
+    for row in iterator:
+        if isinstance(row, list):
+            output += to_regex(row)
+        else:
+            output += row + "\n"
+    return output + ")+"
+
+    
+a = match(first_wrapper_lines, second_html, [], 0, 0)
+print(a)
+print(to_regex(a))
---
+++ pa2/implementation-extraction/utils.py
@@ -0,0 +1,82 @@
+import re
+from bs4 import BeautifulSoup, Comment
+
+REGGEX = "<REGGEX>"
+TAG = "<TAG>"
+STRING = "<STRING>"
+
+def simplify(html):
+    new_html = ""
+    for line in html.split("\n"):
+        if line.startswith("<"):
+            simple = line.split(" ")
+            if simple[0].endswith(">"):
+                new_html += simple[0] + "\n"
+            else:
+                new_html += simple[0] + ">\n"
+        else:
+            new_html += line + "\n"
+    return new_html
+
+def remove_script(html):
+    soup = BeautifulSoup(html, "html.parser")
+    for data in soup(['style', 'script']):
+        # Remove tags
+        data.decompose()
+    
+    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+    [comment.extract() for comment in comments]
+    
+    return str(soup)
+
+def clean_html(html):
+    new_html = ""
+    for i in range(len(html) - 1):
+        if html[i] == ">" or html[i + 1] == "<":
+            new_html += html[i] + "\n"
+        else:
+            new_html += html[i]
+    new_html += html[-1]
+    new_html = new_html.splitlines()
+    new_html = [line for line in new_html if (line and not line.isspace())]
+
+    new_html_joined = []
+    i = 0
+    while i < len(new_html) - 1:
+        tag_name = re.match(r'<\s*([a-zA-Z0-9]+).*?>', new_html[i])
+        if tag_name:
+            tag_name = tag_name.group(1)
+        if tag_name and new_html[i].startswith(("<" + tag_name)) and new_html[i + 1] in ["</" + tag_name + ">"]:
+            new_html_joined.append(new_html[i] + new_html[i + 1])
+            i += 2
+        else:
+            new_html_joined.append(new_html[i])
+            i += 1
+    new_html_joined.append(new_html[-1])
+
+    new_html = "\n".join(new_html_joined)
+
+    return new_html
+
+
+def generate_first_wrapper(cleaned_html):
+    pattern_script = r'<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>'
+    pattern_style = r'<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>'
+    pattern_comment = r'<!--(.*)-->'
+
+    cleaned_html = re.sub(pattern_script, REGGEX + ' <script(?s:.)*?<\/script>', cleaned_html)
+    cleaned_html = re.sub(pattern_style, REGGEX + ' <style(?s:.)*?<\/style>', cleaned_html)
+    cleaned_html = re.sub(pattern_comment, REGGEX + ' <!--(?s:.)*?-->', cleaned_html)
+
+    cleaned_lines = cleaned_html.split("\n")
+    tagged_lines = ""
+    for line in cleaned_lines:
+        if line.startswith(REGGEX):
+            tagged_lines += line
+        elif line.startswith("<") and line.endswith(">"):
+            tagged_lines += TAG + " " + line
+        else:
+            tagged_lines += STRING + " " + line
+        tagged_lines += "\n"
+
+    return tagged_lines
