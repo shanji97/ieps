6b9d97a4fb472f36214f1a2736fef024444acca2
shanji97 <aleksander.kovac97@hotmail.com>
2023-05-23 18:28:37 +0200

    fixed file/folder structure.

---
+++ pa3/implementation-indexing/main.py
@@ -0,0 +1,49 @@
+import os
+import sqlite3
+import time
+
+import utils
+
+if not os.path.exists('inverted-index.db'):
+    conn = sqlite3.connect('inverted-index.db')
+    cursor = conn.cursor()
+    utils.truncate_table(cursor, conn)
+
+    start_time = time.time()
+
+    insert_dict = {}
+    words_set = ()
+    for subdir, dirs, files in os.walk("../sites/e-uprava.gov.si"):
+        for file in files:
+            if file.endswith(".html"):
+
+                filename = os.path.join(subdir, file)
+                print("Inserting from file: " + filename)
+                html_file = open(filename, 'r').read()
+                tokenized_text, normalized_text = utils.get_html_text(html_file)
+
+                text_occurance_count = {}
+                for index, word in enumerate(normalized_text):
+                    if word not in text_occurance_count:
+                        text_occurance_count[word] = {}
+                        text_occurance_count[word]["count"] = 1
+                        text_occurance_count[word]["indexes"] = [index]
+                    else:
+                        text_occurance_count[word]["count"] += 1
+                        text_occurance_count[word]["indexes"].append(index)
+
+                for word in text_occurance_count:
+                    indexes_string = ",".join(
+                        [str(x) for x in text_occurance_count[word]["indexes"]])
+
+                    words_set = words_set + (word,)
+                    insert_dict[(word, filename)] = (
+                        word,
+                        filename,
+                        text_occurance_count[word]["count"],
+                        indexes_string
+                    )
+
+        utils.insert_into_db_many(cursor, conn, insert_dict)
+
+    print("Inserted in: " + str(time.time() - start_time) + " seconds")
---
+++ pa3/implementation-indexing/utils.py
@@ -0,0 +1,56 @@
+import re
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from string import punctuation
+
+def truncate_table(cursor, conn):
+    cursor.execute("DELETE FROM IndexWord")
+    cursor.execute("DELETE FROM Posting")
+    conn.commit()
+
+def insert_into_word_index(cursor, conn, word):
+    res = cursor.execute('SELECT * FROM IndexWord WHERE word=?;', (word,))
+
+    if res.fetchone() is None:
+        cursor.execute('INSERT INTO IndexWord VALUES (?);', (word,))
+        conn.commit()
+
+def insert_into_posting(cursor, conn, word, document_name, frequency, indexes):
+    insert_into_word_index(cursor, conn, word)
+    indexes_string = ",".join([str(x) for x in indexes])
+    cursor.execute('INSERT INTO Posting VALUES (?, ?, ?, ?);',
+                   (word, document_name, frequency, indexes_string))
+    conn.commit()
+
+def insert_into_db_many(cursor, conn, insert_dict):
+    cursor.executemany('INSERT INTO IndexWord VALUES (?);', list(set([(item[0], ) for item in insert_dict.keys()])))
+    cursor.executemany('INSERT INTO Posting VALUES (?, ?, ?, ?);', insert_dict.values())
+    conn.commit()
+
+def get_html_text(html_file):
+    # nltk.download('punkt')
+    soup = BeautifulSoup(html_file, features="html.parser")
+    text = soup.get_text()
+    text = re.sub(re.compile('<.*?>'), '', text)
+
+    # TODO remove punctuation
+    text = text.translate(str.maketrans('', '', punctuation))
+    print(text )
+    # TODO remove stopwords (with nltk, spacy, ...)
+    stopwords = set(stopwords.words('slovene'))
+    
+
+    # TODO remove numbers
+    
+
+    # TODO lemmatize (with classla, nltk, spacy, ...)
+
+    text = '\n'.join([line.strip() for line in text.splitlines() if line.strip()])
+    tokenized_text = word_tokenize(text, language='slovene')
+    tokenized_text_lower = [word.lower() for word in tokenized_text]
+    normalized_text = [word if word not in punctuation else "PUNCT" for word in tokenized_text_lower]
+    print(normalized_text)
+    print(tokenized_text)
+    return tokenized_text, normalized_text
+
