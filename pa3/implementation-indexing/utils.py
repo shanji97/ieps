b923de92cf53719ae9cd40660cdda123a98af9ab
shanji97 <aleksander.kovac97@hotmail.com>
2023-05-23 18:44:17 +0200

    Work on project.

--- pa3/implementation-indexing/utils.py
+++ pa3/implementation-indexing/utils.py
@@ -4,10 +4,17 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from string import punctuation
 
+
+def create_tables(cursor, conn):
+    cursor.execute("CREATE TABLE IndexWord (word TEXT PRIMARY KEY)")
+    cursor.execute("CREATE TABLE Posting (word TEXT NOT NULL, documentName TEXT NOT NULL, frequency INTEGER NOT NULL, indexes TEXT NOT NULL, PRIMARY KEY (word, documentName), FOREIGN KEY (word) REFERENCES IndexWord(word))")
+    conn.commit()
+
 def truncate_table(cursor, conn):
     cursor.execute("DELETE FROM IndexWord")
     cursor.execute("DELETE FROM Posting")
     conn.commit()
+
 
 def insert_into_word_index(cursor, conn, word):
     res = cursor.execute('SELECT * FROM IndexWord WHERE word=?;', (word,))
@@ -16,6 +23,7 @@ def insert_into_word_index(cursor, conn, word):
         cursor.execute('INSERT INTO IndexWord VALUES (?);', (word,))
         conn.commit()
 
+
 def insert_into_posting(cursor, conn, word, document_name, frequency, indexes):
     insert_into_word_index(cursor, conn, word)
     indexes_string = ",".join([str(x) for x in indexes])
@@ -23,10 +31,14 @@ def insert_into_posting(cursor, conn, word, document_name, frequency, indexes):
                    (word, document_name, frequency, indexes_string))
     conn.commit()
 
+
 def insert_into_db_many(cursor, conn, insert_dict):
-    cursor.executemany('INSERT INTO IndexWord VALUES (?);', list(set([(item[0], ) for item in insert_dict.keys()])))
-    cursor.executemany('INSERT INTO Posting VALUES (?, ?, ?, ?);', insert_dict.values())
+    cursor.executemany('INSERT INTO IndexWord VALUES (?);', list(
+        set([(item[0], ) for item in insert_dict.keys()])))
+    cursor.executemany(
+        'INSERT INTO Posting VALUES (?, ?, ?, ?);', insert_dict.values())
     conn.commit()
+
 
 def get_html_text(html_file):
     # nltk.download('punkt')
@@ -36,21 +48,20 @@ def get_html_text(html_file):
 
     # TODO remove punctuation
     text = text.translate(str.maketrans('', '', punctuation))
-    print(text )
+    print(text)
     # TODO remove stopwords (with nltk, spacy, ...)
     stopwords = set(stopwords.words('slovene'))
-    
 
     # TODO remove numbers
-    
 
     # TODO lemmatize (with classla, nltk, spacy, ...)
 
-    text = '\n'.join([line.strip() for line in text.splitlines() if line.strip()])
+    text = '\n'.join([line.strip()
+                     for line in text.splitlines() if line.strip()])
     tokenized_text = word_tokenize(text, language='slovene')
     tokenized_text_lower = [word.lower() for word in tokenized_text]
-    normalized_text = [word if word not in punctuation else "PUNCT" for word in tokenized_text_lower]
+    normalized_text = [
+        word if word not in punctuation else "PUNCT" for word in tokenized_text_lower]
     print(normalized_text)
     print(tokenized_text)
     return tokenized_text, normalized_text
-
